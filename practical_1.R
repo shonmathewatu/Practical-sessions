b0f535f3effeaa54efd4561d42fac89e477b8ebd
unknown <l00171122@atu.ie>
2023-03-09 12:29:11 +0000

    practical session 1

---
+++ practical_1.R
@@ -0,0 +1,19 @@
+x <- c(3,12,7)
+y <- c(4,9,11)
+z = x + y
+print(z)
+
+
+#multply each element by 2
+
+new_z <- 2*z +100
+
+print(new_z)
+
+my_sqrt = sqrt(new_z-1)
+
+print(my_sqrt)
+
+a = c(1,2,3,4) + c(0,10,3)
+
+print(a)
